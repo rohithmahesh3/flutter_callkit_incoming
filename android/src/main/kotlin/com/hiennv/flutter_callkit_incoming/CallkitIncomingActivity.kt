82fc09bc40619331d46dae4f6f4ce48e06117572
Rohith Mahesh <rohith@tequerist.com>
2022-06-28 20:55:45 +0530

    onAcceptClick changed

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
@@ -271,19 +271,15 @@ class CallkitIncomingActivity : Activity() {
 
     private fun onAcceptClick() {
         val data = intent.extras?.getBundle(EXTRA_CALLKIT_INCOMING_DATA)
-        val intent = packageManager.getLaunchIntentForPackage(packageName)?.cloneFilter()
-        if (isTaskRoot) {
-            intent?.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        } else {
-            intent?.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        }
-        if (intent != null) {
-            val intentTransparent = TransparentActivity.getIntentAccept(this@CallkitIncomingActivity, data)
-            startActivities(arrayOf(intent, intentTransparent))
-        } else {
-            val acceptIntent = CallkitIncomingBroadcastReceiver.getIntentAccept(this@CallkitIncomingActivity, data)
-            sendBroadcast(acceptIntent)
-        }
+        packageManager.getLaunchIntentForPackage(packageName)?.cloneFilter()?.apply {
+            if(isTaskRoot){
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            } else {
+                intent?.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
+        }?.let { startActivity(intent) }
+        val acceptIntent = CallkitIncomingBroadcastReceiver.getIntentAccept(this@CallkitIncomingActivity, data)
+        sendBroadcast(acceptIntent)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
             keyguardManager.requestDismissKeyguard(this, null)
